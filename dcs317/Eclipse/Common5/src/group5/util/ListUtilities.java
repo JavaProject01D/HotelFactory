4a38df5cfd0ff8be3c425bed87f88322d44b9d0f
1532444 <1532444@2F16-32.ad.dawsoncollege.qc.ca>
2016-10-12 14:16:51 -0400

    Getting ready for Part 1 and 2, nothing special yet

--- dcs317/Eclipse/Common5/src/group5/util/ListUtilities.java
+++ dcs317/Eclipse/Common5/src/group5/util/ListUtilities.java
@@ -1,8 +1,14 @@
 package group5.util;
 
+import java.io.IOException;
+
 public class ListUtilities{
 	
-	public ListUtilities(){}
+	/*
+	 * to prevent instantiation of the class
+	 */
+	private ListUtilities(){}
+	
 	/*
 	 * Sorts a list of objects in ascending natural order using 
 	 * selection sort.
@@ -26,9 +32,7 @@ public class ListUtilities{
 		int index;
 		for(int i=0; i < list.length; i++){
 			index =i;
-			for(int k=1; k < list.length; k++){
-				if(list[i] == null)
-					throw new NullPointerException("There is a null element");
+			for(int k=1; k < list.length; k++){			
 				
 				if(list[i].compareTo(list[k]) > 1)
 					index = k;
@@ -36,4 +40,71 @@ public class ListUtilities{
 			list[i] = list[index];
 		}
 	}
+	
+	//SEE LAB 7
+	private static void saveListToTextFile(){
+		
+	}
+	
+	/*
+	 * Sorts a list of objects in ascending natural order using 
+	 * selection sort.
+	 * 
+	 * Precondition: 	Assumes that the list is not null and that the 
+	 *	list's capacity is equal to the list's size.
+	 * 
+	 *
+	 * @param list 	A list of objects. Assumes that the
+	 *             	list's capacity is equal to the list's size. 
+	 * 
+	* @throws  		IllegalArgumentException if the parameter is      *			not full to capacity.
+	*
+	* @throws		NullPointerException if the list is null.
+	 */
+		/* @SuppressWarnings({ "rawtypes", "unchecked" })
+		 public static void sort(Comparable[] list)
+					throws IllegalArgumentException, NullPointerException{
+			 
+			 
+		 }*/
+	
+	
+	
+	/*
+	 * Efficiently merges two sorted lists of objects in ascending 
+	 * natural order. If the duplicate objects are in both lists, 
+	 * the object from list1 is merged into the resulting list, and
+	 * both objects are written to the duplicate file. 
+	 * 
+	 * Precondition: 	Assumes that the lists are not null and that 
+	 *           both lists contain objects that can be compared to 
+	 *           each other and are filled to capacity.
+	 * 
+	 *
+	 * @param list1 	A naturally sorted list of objects. Assumes 
+	 *                that the list contains no duplicates and that 
+	 *                its capacity is equal to its size. 
+	 * @param list2 	A naturally sorted list of objects. Assumes 
+	 *                that the list contains no duplicates and that 
+	 *                its capacity is equal to its size. 
+	 * @param duplicateFileName  The name of the file in 
+	 *                datafiles\duplicates to which duplicate pairs 
+	 *                will be appended.
+	 * 
+	 * @throws        IllegalArgumentException if either parameter is
+	 *                not full to capacity.
+	 *
+	 * @throws		NullPointerException if the either list is 
+	 *                null.
+	 */
+
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			public static Comparable[] merge(Comparable[] list1,
+				Comparable[] list2, String duplicateFileName)
+					throws IOException{
+				
+				return null;
+			}
+
+	
 }
---
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/HotelFileLoader.java
@@ -0,0 +1,33 @@
+package group5.hotel.data;
+
+import java.io.IOException;
+
+import dw317.hotel.business.interfaces.Customer;
+import dw317.hotel.business.interfaces.Room;
+
+/**
+ * @author Denis Lebedev
+ *
+ */
+public class HotelFileLoader {
+
+	private HotelFileLoader(){
+		
+	}
+	
+	public static Room[] getRoomListFromSequentialFile(String filename)
+			throws IOException{
+		
+		
+		
+		return true;
+	}
+	
+	public static Customer[] getCustomerListFromSequentialFile(String filename)
+			throws IOException{
+		
+		return true;
+	}
+
+
+}
