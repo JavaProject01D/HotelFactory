c7f2202241762375857ffa2776e333b327d59b68
1532444 <1532444@2F16-07.ad.dawsoncollege.qc.ca>
2016-10-18 11:19:35 -0400

    Starting HotelFileLoader

--- dcs317/Eclipse/ReservationSys/src/group5/hotel/data/HotelFileLoader.java
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/HotelFileLoader.java
@@ -1,6 +1,10 @@
 package group5.hotel.data;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.sun.java_cup.internal.runtime.Scanner;
 
 import dw317.hotel.business.interfaces.Customer;
 import dw317.hotel.business.interfaces.Room;
@@ -18,15 +22,36 @@ public class HotelFileLoader {
 	public static Room[] getRoomListFromSequentialFile(String filename)
 			throws IOException{
 		
+		Scanner inputStream = null;
+		String record = null;
 		
 		
-		return true;
+		
+		try{
+			
+			BufferedReader outStream = new BufferedReader(new InputStreamReader
+					new FileInputStream("rooms.txt"), Standard(harsets.UTF_8));
+			
+			inputStream = new Scanner(outStream);
+			
+			int i =0;
+			
+			while(inputStream.hasNext()){
+				
+			}
+			
+		}catch(IllegalArgumentException iae){
+			
+		}
+		
+		
+		return null;
 	}
 	
 	public static Customer[] getCustomerListFromSequentialFile(String filename)
 			throws IOException{
 		
-		return true;
+		return null;
 	}
 
 
