8b67bea14013c239b5571535b6ae817107e11c67
1532444 <1532444@2F16-31.ad.dawsoncollege.qc.ca>
2016-10-21 12:21:47 -0400

    Adding new data files and Part1 is close to finish

--- dcs317/Eclipse/ReservationSys/src/group5/hotel/data/HotelFileLoader.java
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/HotelFileLoader.java
@@ -1,13 +1,15 @@
 package group5.hotel.data;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import com.sun.java_cup.internal.runtime.Scanner;
-
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
 import dw317.hotel.business.interfaces.Customer;
 import dw317.hotel.business.interfaces.Room;
+import group5.hotel.business.DawsonHotelFactory;
 
 /**
  * @author Denis Lebedev
@@ -22,36 +24,100 @@ public class HotelFileLoader {
 	public static Room[] getRoomListFromSequentialFile(String filename)
 			throws IOException{
 		
-		Scanner inputStream = null;
+		Room [] arr = new Room[2];
+		
+		Scanner inputStream = null ;
 		String record = null;
-		
-		
+		DawsonHotelFactory dHF = DawsonHotelFactory.DAWSON;
 		
 		try{
 			
-			BufferedReader outStream = new BufferedReader(new InputStreamReader
-					new FileInputStream("rooms.txt"), Standard(harsets.UTF_8));
+			BufferedReader outStream = new BufferedReader(new InputStreamReader(
+					new FileInputStream(filename), StandardCharsets.UTF_8));
 			
 			inputStream = new Scanner(outStream);
 			
 			int i =0;
 			
 			while(inputStream.hasNext()){
+				record = inputStream.nextLine();
+				String [] fields = record.split("\\*");
 				
-			}
+				try{
+					
+					//CATCH PARSE EXCEPTION!!!
+					arr[i] = dHF.getRoomInstance(Integer.parseInt(fields[i]), fields[i]);
+					
+				}catch(IllegalArgumentException iae){
+					System.out.println(iae.getMessage() + "\nFileName: " + filename + "\nRecord: " + record);
+				}
+				
+				if(i > arr.length) // resize
+					arr = Arrays.copyOf(arr, arr.length * 2 + 1);
+				
+			}//end of the while loop
 			
-		}catch(IllegalArgumentException iae){
-			
-		}
+			//shrink
+			arr = Arrays.copyOf(arr,i);
+				
+		}catch(IOException io){		}
+		
+		//Close Scanner
+		finally{ inputStream.close();}
 		
 		
-		return null;
+		return arr;
 	}
 	
 	public static Customer[] getCustomerListFromSequentialFile(String filename)
 			throws IOException{
 		
-		return null;
+		Customer [] arr = new Customer[2];
+		
+		Scanner inputStream = null ;
+		String record = null;
+		DawsonHotelFactory dHF = DawsonHotelFactory.DAWSON;
+		
+		try{
+			
+			BufferedReader outStream = new BufferedReader(new InputStreamReader(
+					new FileInputStream(filename), StandardCharsets.UTF_8));
+			
+			inputStream = new Scanner(outStream);
+			
+			int i =0;
+			
+			while(inputStream.hasNext()){
+				record = inputStream.nextLine();
+				String [] fields = record.split("\\*");
+				
+				try{
+					
+					//first, last, email - instance
+					//email,FN,LN,Card,Card#
+					//IF ** = they are ignored
+					//IF *''*'' = ERROR THROW
+					arr[i] = dHF.getCustomerInstance();
+					
+				}catch(IllegalArgumentException iae){
+					System.out.println(iae.getMessage() + "\nFileName: " + filename + "\nRecord: " + record);
+				}
+				
+				if(i > arr.length) // resize
+					arr = Arrays.copyOf(arr, arr.length * 2 + 1);
+				
+			}//end of the while loop
+			
+			//shrink
+			arr = Arrays.copyOf(arr,i);
+				
+		}catch(IOException io){		}
+		
+		//Close Scanner
+		finally{ inputStream.close();}
+		
+		
+		return arr;
 	}
 
 
