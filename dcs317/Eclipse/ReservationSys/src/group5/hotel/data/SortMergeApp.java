767aefe41b05642cd3e9d08479933a6c96f3f286
1537595 <1537595@2F16-32.ad.dawsoncollege.qc.ca>
2016-10-21 17:01:26 -0400

    5:00pm changes SortMergeApp

---
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/SortMergeApp.java
@@ -0,0 +1,54 @@
+package group5.hotel.data;
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * @author Zahraa
+ *
+ */
+public class SortMergeApp {
+	
+	public static void main (String[] args) {
+		
+	
+		File database = new File ("datafiles");
+		database.mkdir();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*
+	public static Comparable[] Merge(Comparable[] list1, 
+									 Comparable[] list2 )  {
+										 
+		Comparable[] list3 = new Comparable[list1.length + list2.length];								
+		
+		int index = 0; 
+		
+		for (int i = 0; i < list3.length; i++ ) {
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		return null;
+		
+		
+		
+										 
+	 }
+*/
+	
+}
