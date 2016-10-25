6c881b26a586a9820e91eff4e66fad106b95765d
1537595 <1537595@2F16-08.ad.dawsoncollege.qc.ca>
2016-10-25 14:06:51 -0400

    merge

--- dcs317/Eclipse/ReservationSys/src/group5/hotel/data/SortMergeApp.java
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/SortMergeApp.java
@@ -18,39 +18,16 @@ public class SortMergeApp {
 	
 	public static void main (String[] args) {
 		
-	
-		File database = new File ("datafiles");
-		database.mkdir();
-		//Rooms
-		Room[] rooms;
-		
-		try {
-			rooms = HotelFileLoader.getRoomListFromSequentialFile("rooms.txt");	
-			//Sorting
-			ListUtilities.sort(rooms);
-			
-			} catch (IOException e) {
-			System.out.println(e.getMessage()); //??
-		}
-	
-		if (database.exists()) 
-			try {
-				
-				File room = new File (database + "/rooms.txt");
-				// Write sortedRooms array to the file 
-				} catch (IOException e) {	
-					
-				System.out.print("Could not create file.." + e.getMessage());		
-				
-				}
-		
 		File sorted = new File ("datafiles");
 		sorted.mkdir();
 		
 		//Customers
 		Customer[] customers;
-
-		for (int i = 1; i <= 10; i++){
+		Customer[][] customerList;
+	
+		int k = 0;
+		
+		for (int i = 1; i < 11; i++){
 			
 			String customerFileName = "customers" + i + ".txt";
 			String sortedCustFileName = "sortedCustomers" + i + ".txt";
@@ -60,7 +37,8 @@ public class SortMergeApp {
 			customers = HotelFileLoader.getCustomerListFromSequentialFile(customerFileName);
 			//Sorting
 			ListUtilities.sort(customers);
-			
+			customerList[k] = customers;
+			k++;
 			} catch (IOException e) {
 				
 			System.out.println(e.getMessage()); //??
@@ -73,6 +51,7 @@ public class SortMergeApp {
 				
 				File room = new File (sorted + sortedCustFileName);
 				//Write sortedCustomers array to the file
+				ListUtilities.saveListToTextFile(customerList[i], sortedCustFileName);
 				
 				} catch (IOException e) {	
 					
@@ -113,55 +92,55 @@ public class SortMergeApp {
 				}
 	
 		} 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			
 		
 	} // end of main method
 	
-	
-	
-	
-	
-	
-	
-	
-	
-/*
-	public static Comparable[] Merge(Comparable[] list1, 
-									 Comparable[] list2 )  {
-										 
-		Comparable[] list3 = new Comparable[list1.length + list2.length];								
+	public static void sortedRooms(){
 		
-		int index = 0; 
+		File database = new File ("datafiles");
+		database.mkdir();
+		//Rooms
+		Room[] rooms;
 		
-		for (int i = 0; i < list3.length; i++ ) {
+		try {
+			rooms = HotelFileLoader.getRoomListFromSequentialFile("rooms.txt");	
+			//Sorting
+			ListUtilities.sort(rooms);
 			
-			
-			
+			} catch (IOException e) {
+			System.out.println(e.getMessage()); //??
 		}
+	
+		if (database.exists()) 
+			try {
+				rooms = HotelFileLoader.getRoomListFromSequentialFile("rooms.txt");	
+
+				File room = new File (database + "/rooms.txt");
+				// Write sortedRooms array to the file 
+				ListUtilities.saveListToTextFile(rooms, "rooms.txt");
+				
+				} catch (IOException e) {	
+					
+				System.out.print("Could not create file.." + e.getMessage());		
+				
+				}
 		
 		
 		
 		
 		
-		return null;
+		File sorted = new File ("datafiles");
+		sorted.mkdir();
 		
 		
-		
-										 
-	 }
-*/
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
