59042277ac51841232f8a98bbba7bcbdf13c1d7d
Sevan Topalian <sevan.topalian@gmail.com>
2016-11-09 00:23:17 -0500

    Finished part 2 (added the code), started parts 1 and 3

---
+++ dcs317/Eclipse/ReservationSys/src/dw317/hotel/data/DuplicateReservationException.java
@@ -0,0 +1,5 @@
+package dw317.hotel.data;
+
+public class DuplicateReservationException extends Exception {
+	private static final long  serialVersionUID = 42031768871L;
+}
---
+++ dcs317/Eclipse/ReservationSys/src/dw317/hotel/data/NonExistingReservationException.java
@@ -0,0 +1,5 @@
+package dw317.hotel.data;
+
+public class NonExistingReservationException extends Exception {
+	private static final long  serialVersionUID = 42031768871L;
+}
