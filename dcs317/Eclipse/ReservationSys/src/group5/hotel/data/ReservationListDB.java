d0f9c29c01d35a6a862458906d08bed4d34f2316
Zahraa <Zahraa@25.110.238.213>
2016-11-13 09:13:32 -0500

    everything done except one method clearAllPast() did not know how to do it so check it out please

--- dcs317/Eclipse/ReservationSys/src/group5/hotel/data/ReservationListDB.java
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/data/ReservationListDB.java
@@ -18,36 +18,62 @@ import group5.hotel.business.DawsonHotelFactory;
 import group5.hotel.business.DawsonReservation;
 
 /**
- * @author 1537595
- *
+ * 
+ * @author Zahraa Horeibi
+ * @version 11/13/2016
  */
 public class ReservationListDB implements ReservationDAO {
-	
+
 	private List<Reservation> database;
 	private List<Room> allRooms;
 	private final ListPersistenceObject listPersistenceObject;
-	private final HotelFactory factory; 
-		
-	public ReservationListDB(ListPersistenceObject listPersistenceObject){
-		
+	private final HotelFactory factory;
+
+	/**
+	 * We use the ListPersistenceObject to load our database for needed data. We
+	 * do not have a HotelFactory given so we set a default factory by using
+	 * DawsonHotelFactory
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param listPersistenceObject
+	 * 
+	 */
+
+	public ReservationListDB(ListPersistenceObject listPersistenceObject) {
+
 		this.listPersistenceObject = listPersistenceObject;
 		this.allRooms = this.listPersistenceObject.getRoomDatabase();
 		this.database = this.listPersistenceObject.getReservationDatabase();
 		factory = DawsonHotelFactory.DAWSON;
 	}
-	
-	public ReservationListDB (ListPersistenceObject listPersistenceObject,
-			HotelFactory factory) {
-		
+
+	/**
+	 * We use the ListPersistenceObject to load our database for needed data.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param listPersistenceObject
+	 * @param factory
+	 * 
+	 */
+
+	public ReservationListDB(ListPersistenceObject listPersistenceObject, HotelFactory factory) {
+
 		this.listPersistenceObject = listPersistenceObject;
 		this.allRooms = this.listPersistenceObject.getRoomDatabase();
 		this.factory = factory;
-		
+
 	}
 
+	/**
+	 * Overridden toString method so that when invoked, it returns a String
+	 * representation of the contents of the database, one element per line.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @return String
+	 */
 	@Override
 	public String toString() {
-		
+
 		int num = database.size();
 		StringBuilder str = new StringBuilder("\nNumber of reservations in database: " + num);
 		for (Reservation r : database) {
@@ -55,112 +81,196 @@ public class ReservationListDB implements ReservationDAO {
 		}
 		return str.toString();
 	}
-	
-	
-	@Override
-	public void add(Reservation reserv) throws DuplicateReservationException {
-			// Creating a deep copy of reserv
-			Reservation copyReserv = factory.getReservationInstance(reserv);	
-			
-			int index = binarySearch(this.database, copyReserv);
-				index = -(index);
-				
-				if (index > 0)
-					throw new DuplicateReservationException();
-			//binary search so we can insert it at the correct position (not sure)
-			database.add(index,copyReserv);			
-			
-	}
+
+	/**
+	 * Overridden add method which first checks if the reservation overlaps with
+	 * an existing reservation. If there is an overlap, throws a
+	 * DuplicateReservationExcption. If the reservation does not overlap, adds a
+	 * reference to a copy of the object referenced by the reserv and not the
+	 * actual object being referenced by the parameter.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param reserv
+	 * @throws DuplicateReservationException
+	 * 
+	 */
 
 	@Override
+	public void add(Reservation reserv) throws DuplicateReservationException {
+		// Creating a deep copy of reserv
+		Reservation copyReserv = factory.getReservationInstance(reserv);
+
+		int index = binarySearch(this.database, copyReserv);
+		index = -(index);
+
+		if (index > 0)
+			throw new DuplicateReservationException();
+		// binary search so we can insert it at the correct position (not sure)
+		database.add(index, copyReserv);
+
+	}
+
+	/**
+	 * This method saves the database to disk and assign null to the database
+	 * field.
+	 * 
+	 * @author Zahraa
+	 * @throws IOException
+	 */
+	@Override
 	public void disconnect() throws IOException {
-		
+
 		try {
 			this.listPersistenceObject.saveReservationDatabase(this.database);
-			} catch ( IOException ie ) {
+		} catch (IOException ie) {
 			throw new IOException("Error saving to file!");
 		}
 		this.database = null;
 	}
 
+	/**
+	 * Returns a copy of the Reservations belonging to the given customer, or an
+	 * empty ArrayList.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param cust
+	 * @return ArrayList
+	 */
 	@Override
 	public ArrayList<Reservation> getReservations(Customer cust) {
-		
+
 		ArrayList<Reservation> res = new ArrayList<Reservation>();
-		
-		for ( int i = 0; i < database.size(); i++) {	
-			for ( Reservation r : this.database) {
+
+		for (int i = 0; i < database.size(); i++) {
+			for (Reservation r : this.database) {
 				if (r.getCustomer().equals(cust)) {
 					res.set(i, r);
-				}	
+				}
 			}
-		}			
+		}
 		return res;
 	}
+
+	/**
+	 * Removes the given reservation from the database if it is found; otherwise
+	 * throw a NonExistingReservationException.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param reserv
+	 * @throws NonExistingReservationException
+	 */
 
 	@Override
 	public void cancel(Reservation reserv) throws NonExistingReservationException {
 		int index = binarySearch(this.database, reserv);
-			if (index > 0) 
-				this.database.remove(index);	
-			if (index < 0)  
-				throw new NonExistingReservationException();		
+		if (index > 0)
+			this.database.remove(index);
+		if (index < 0)
+			throw new NonExistingReservationException();
 	}
+
+	/**
+	 * Returns an ArrayList with all reserved Rooms overlapping during the time
+	 * period. You must check if an existing reservation has a room that has
+	 * been reserved with the checkin date of the reservation before the
+	 * checkout date provided, and the checkout date of the reservation is after
+	 * the checkin date provided: in this case the room is reserved.
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param checkin
+	 * @param checkout
+	 * @return ArrayList
+	 */
 
 	@Override
 	public ArrayList<Room> getReservedRooms(LocalDate checkin, LocalDate checkout) {
-		
+
 		ArrayList<Room> rooms = new ArrayList<Room>();
-		
-		for ( int i = 0; i < database.size(); i++) {	
-			for ( Reservation r : this.database) {
+
+		for (int i = 0; i < database.size(); i++) {
+			for (Reservation r : this.database) {
 				if (checkin.isBefore(checkout) && checkout.isAfter(checkin)) {
-					if ( r.getCheckInDate().equals(checkin) && r.getCheckOutDate().equals(checkout)){
+					if (r.getCheckInDate().equals(checkin) && r.getCheckOutDate().equals(checkout)) {
 						rooms.set(i, r.getRoom());
 					}
-				}	
+				}
 			}
 		}
 		return rooms;
 	}
 
+	/**
+	 * Returns an ArrayList with all unreserved Rooms overlapping during the
+	 * time period
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param checkin
+	 * @param checkout
+	 * @return ArrayList
+	 */
 	@Override
 	public ArrayList<Room> getFreeRooms(LocalDate checkin, LocalDate checkout) {
 		ArrayList<Room> emptyRooms = new ArrayList<Room>();
-		
-		for ( int i = 0; i < database.size(); i++) {		
-			for ( Reservation r : this.database) {
-				if (!checkin.isBefore(checkout) && !checkout.isAfter(checkin)) {
-						emptyRooms.set(i, r.getRoom());	
-				}	
-			}
-		}	
-			return emptyRooms;
-	}
 
-	@Override
-	public List<Room> getFreeRooms(LocalDate checkin, LocalDate checkout, RoomType roomType) {
-		ArrayList<Room> emptyRooms = new ArrayList<Room>();
-		
-		for ( int i = 0; i < database.size(); i++) {		
-			for ( Reservation r : this.database) {
-				if ( r.getRoom().equals(roomType) && 
-					  !checkin.isBefore(checkout) && !checkout.isAfter(checkin)) {
-						emptyRooms.set(i, r.getRoom());	
-				}	
+		for (int i = 0; i < database.size(); i++) {
+			for (Reservation r : this.database) {
+				if (!checkin.isBefore(checkout) && !checkout.isAfter(checkin)) {
+					emptyRooms.set(i, r.getRoom());
+				}
 			}
-		}	
+		}
 		return emptyRooms;
 	}
 
+	/**
+	 * Returns an ArrayList with all unreserved Rooms with the given room type
+	 * overlapping during the time period
+	 * 
+	 * @author Zahraa Horeibi
+	 * @param checkin
+	 * @param checkout
+	 * @param roomType
+	 * @return ArrayList
+	 */
+	@Override
+	public ArrayList<Room> getFreeRooms(LocalDate checkin, LocalDate checkout, RoomType roomType) {
+		ArrayList<Room> emptyRooms = new ArrayList<Room>();
+
+		for (int i = 0; i < database.size(); i++) {
+			for (Reservation r : this.database) {
+				if (r.getRoom().equals(roomType) && !checkin.isBefore(checkout) && !checkout.isAfter(checkin)) {
+					emptyRooms.set(i, r.getRoom());
+				}
+			}
+		}
+		return emptyRooms;
+	}
+
+	/**
+	 * This method removes all Reservations whose checkout date is before the
+	 * current date
+	 * 
+	 * @author Zahraa Horeibi
+	 */
+
 	@Override
 	public void clearAllPast() {
-		for ( Reservation r : this.database ) {
+		for (Reservation r : this.database) {
 			if (r.getCheckOutDate().isBefore(LocalDate.now())) {
-				// ?? 
+				// ??
 			}
-		}			
+		}
 	}
+
+	/**
+	 * This method is a helper method which returns the index of a given
+	 * reservation inside of the reservation list (database) using a binary
+	 * search.
+	 * 
+	 * @param reservationList
+	 * @param res
+	 * @return int
+	 */
 
 	private static int binarySearch(List<? extends Reservation> reservationList, Reservation res) {
 
@@ -169,24 +279,18 @@ public class ReservationListDB implements ReservationDAO {
 		int mid = (low + high) / 2;
 		int result;
 
+		while (low <= high) {
 
-		while ( low <= high) {
-			
 			mid = (low + high) / 2;
 			result = reservationList.get(mid).compareTo(res);
-			
+
 			if (result == 0) {
 				return mid;
-			}
-			else if (result < 0) {
+			} else if (result < 0) {
 				low = mid + 1;
-			}
-			else 
-				high = mid - 1;	
-		}	
+			} else
+				high = mid - 1;
+		}
 		return -(high + 1);
 	}
 }
-	
-	
-	
--- dcs317/Eclipse/ReservationSys/test/group5/hotel/data/ReservationListDBTest.java
+++ dcs317/Eclipse/ReservationSys/test/group5/hotel/data/ReservationListDBTest.java
@@ -2,13 +2,16 @@ package group5.hotel.data;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import dw317.hotel.business.RoomType;
 import dw317.hotel.business.interfaces.Customer;
 import dw317.hotel.business.interfaces.Reservation;
 import dw317.hotel.business.interfaces.Room;
-
+import dw317.hotel.data.DuplicateCustomerException;
 import dw317.hotel.data.DuplicateReservationException;
+import dw317.hotel.data.NonExistingCustomerException;
+import dw317.hotel.data.NonExistingReservationException;
 import group5.hotel.business.DawsonCustomer;
 import group5.hotel.business.DawsonReservation;
 import group5.hotel.business.DawsonRoom;
@@ -18,13 +21,17 @@ public class ReservationListDBTest {
 
 	public static void main(String[] args) {
 		testAdd();
+		testDisconnect();
+		testCancel();
+		testGetReservations();
+		testGetReservedRooms();
+		testGetFreeRooms();
+		testThreeParamsGetFreeRooms();
 
-		
-		
 	}
-	
+
 	private static void setup() {
-		
+
 		String[] rooms = new String[8];
 		rooms[0] = "206*normal";
 		rooms[1] = "401*normal";
@@ -34,47 +41,40 @@ public class ReservationListDBTest {
 		rooms[5] = "704*suite";
 		rooms[6] = "801*penthouse";
 		rooms[7] = "801*penthouse";
-		
-		String[] custs = new String[8];
-		custs [0] = "bob.b.y_lee@hotmail.ca*Bobby*Lee*mastercard*5458325441641567";
-		custs [1] = "madoriHu@host.com*Humico*Madori*mastercard*5233382411178726";
-		custs [2] = "Jean_Gero@gg.fr*Jean*Gerophar*visa*4929596474756407";
-		custs [3] = "MyNameIsSheila@localhost*Sheila*Kaif*AMEX*374425782767815";
-		custs [4] = "cuty.kathy.2008@yahoo.ca*Kathy*Perry**";
-		custs [5] = "robert.is-awesome@mail.me*Robert*Siri*Visa*4485011762777210";
-		custs [6] = "john.bussiness@gmail.ca*Johny-Laurence*Smith**";
-		custs [7] = "Umila-Gangee@local.ca*Umila*Gangee**";
-		
-		
 
-		String[] reservs = new String[8];	
-		reservs [0] = "bob.b.y_lee@hotmail.ca*2016*8*30*2016*12*25*206";
-		reservs [1] = "madoriHu@host.com*2016*10*26*2016*12*30*401";
-		reservs [2] = "Jean_Gero@gg.fr*2016*11*28*2017*2*25*406";
-		reservs [3] = "MyNameIsSheila@localhost*2017*5*9*2017*6*1*503";
-		reservs [4] = "cuty.kathy.2008@yahoo.ca*2016*12*25*2017*1*1*601";
-		reservs [5] = "robert.is-awesome@mail.me*2017*1*1*2018*1*1*704";
-		reservs [6] = "john.bussiness@gmail.ca*2016*9*20*2016*9*26*801";
-		reservs [7] = "Umila-Gangee@local.ca*2017*5*5*2017*7*8*801";
-		
+		String[] custs = new String[8];
+		custs[0] = "bob.b.y_lee@hotmail.ca*Bobby*Lee*mastercard*5458325441641567";
+		custs[1] = "madoriHu@host.com*Humico*Madori*mastercard*5233382411178726";
+		custs[2] = "Jean_Gero@gg.fr*Jean*Gerophar*visa*4929596474756407";
+		custs[3] = "MyNameIsSheila@localhost*Sheila*Kaif*AMEX*374425782767815";
+		custs[4] = "cuty.kathy.2008@yahoo.ca*Kathy*Perry**";
+		custs[5] = "robert.is-awesome@mail.me*Robert*Siri*Visa*4485011762777210";
+		custs[6] = "john.bussiness@gmail.ca*Johny-Laurence*Smith**";
+		custs[7] = "Umila-Gangee@local.ca*Umila*Gangee**";
+
+		String[] reservs = new String[8];
+		reservs[0] = "bob.b.y_lee@hotmail.ca*2016*8*30*2016*12*25*206";
+		reservs[1] = "madoriHu@host.com*2016*10*26*2016*12*30*401";
+		reservs[2] = "Jean_Gero@gg.fr*2016*11*28*2017*2*25*406";
+		reservs[3] = "MyNameIsSheila@localhost*2017*5*9*2017*6*1*503";
+		reservs[4] = "cuty.kathy.2008@yahoo.ca*2016*12*25*2017*1*1*601";
+		reservs[5] = "robert.is-awesome@mail.me*2017*1*1*2018*1*1*704";
+		reservs[6] = "john.bussiness@gmail.ca*2016*9*20*2016*9*26*801";
+		reservs[7] = "Umila-Gangee@local.ca*2017*5*5*2017*7*8*801";
+
 		File dir = new File("testfiles");
-		try{
-			if (!dir.exists()){  
+		try {
+			if (!dir.exists()) {
 				dir.mkdirs();
 			}
-			ListUtilities.saveListToTextFile(rooms, 
-					"testfiles/testRooms.txt");
-			ListUtilities.saveListToTextFile(custs, 
-					"testfiles/testCustomers.txt");
-			ListUtilities.saveListToTextFile(reservs, 
-					"testfiles/testReservations.txt");
-		}
-		catch(IOException io){
-			System.out.println
-			("Error creating file in setUp()");
+			ListUtilities.saveListToTextFile(rooms, "testfiles/testRooms.txt");
+			ListUtilities.saveListToTextFile(custs, "testfiles/testCustomers.txt");
+			ListUtilities.saveListToTextFile(reservs, "testfiles/testReservations.txt");
+		} catch (IOException io) {
+			System.out.println("Error creating file in setUp()");
 		}
 	}
-	
+
 	private static void teardown() {
 		File theFile = new File("testfiles/testRooms.txt");
 		if (theFile.exists()) {
@@ -89,74 +89,281 @@ public class ReservationListDBTest {
 			theFile.delete();
 		}
 	}
-	
+
 	private static void testAdd() {
 		setup();
-		SequentialTextFileList file = new SequentialTextFileList
-				("testfiles/testRooms.txt", "testfiles/testCustomers.txt",
-						"testfiles/testReservations.txt");
-		
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+
 		ReservationListDB db = new ReservationListDB(file);
 		System.out.println(db.toString());
-		
+		System.out.println("________________________ TESTING add() _______________________\n");
+
 		String[] testcase = new String[6];
-	
+
 		Reservation[] resToAdd = new DawsonReservation[6];
 
 		testcase[0] = " Case 1: Valid Data";
-		resToAdd[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"), new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
-				
-		testcase[1] = " Case 2: Invalid Data: Customer already in list";
-		resToAdd[1] = new DawsonReservation(new DawsonCustomer("Humico", "Madori", "madoriHu@host.com"), new DawsonRoom(401, RoomType.NORMAL), 2016, 10, 26, 2016, 12, 30);
+		resToAdd[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
 
-		for(int i=0; i < resToAdd.length; i++){
+		testcase[1] = " Case 2: Invalid Data: Reservation already in list";
+		resToAdd[1] = new DawsonReservation(new DawsonCustomer("Humico", "Madori", "madoriHu@host.com"),
+				new DawsonRoom(401, RoomType.NORMAL), 2016, 10, 26, 2016, 12, 30);
+
+		for (int i = 0; i < resToAdd.length; i++) {
 			System.out.println(testcase[i]);
-			
-			try{
-				db.add(resToAdd[i]);				
-			}catch(DuplicateReservationException dce){
+
+			try {
+				db.add(resToAdd[i]);
+			} catch (DuplicateReservationException dce) {
 				System.out.println("DuplicateReservation: " + dce.getMessage());
 				continue;
-			}catch(Exception e){
-				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> " );
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
 				continue;
 			}
-			
+
 		}
 		System.out.println("\n==== List ====");
 		System.out.println(db.toString());
-		
-	teardown();
-		
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-	//new ReservationListDB(this.listPersistenceObject, this.factory);
-	
-	
-	/*private static void testToString() {
-		setup();
+		teardown();
+
 	}
-	*/
-	
-	
-	
-	
-	
-	
+
+	private static void testDisconnect() {
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println("________________________ TESTING disconnect() _______________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		Reservation res = new DawsonReservation(new DawsonCustomer("Hum", "Mad", "mad@host.com"),
+				new DawsonRoom(602, RoomType.NORMAL), 2016, 10, 26, 2016, 12, 30);
+
+		try {
+			System.out.println("\nAdding a customer then Disconect: ");
+			db.add(res);
+
+			System.out.println(db.toString());
+
+			System.out.println("\nDisconect--->");
+			db.disconnect();
+		} catch (DuplicateReservationException dre) {
+			System.out.println("DuplicateReservationException: " + dre.getMessage());
+		} catch (IOException ioe) {
+			System.out.println("IOE Exception: " + ioe.getMessage());
+		} catch (Exception e) {
+			System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+		}
+
+		System.out.println("\nReconnect--->");
+		ReservationListDB dbTwo = new ReservationListDB(file);
+
+		System.out.println(dbTwo.toString());
+
+		teardown();
+	}
+
+	private static void testCancel() {
+
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println("________________________ TESTING cancel() _______________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		String[] testcase = new String[6];
+		Reservation[] resToAdd = new DawsonReservation[6];
+
+		testcase[0] = "\n Case 1: Valid Data";
+		resToAdd[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
+
+		testcase[1] = "\n Case 2: Invalid Data: Reservation not in list";
+		resToAdd[1] = new DawsonReservation(new DawsonCustomer("Dominica", "Esperente", "piquanteChiquita@hotmail.com"),
+				new DawsonRoom(605, RoomType.NORMAL), 2016, 10, 26, 2016, 12, 30);
+
+		for (int i = 0; i < resToAdd.length; i++) {
+			System.out.println(testcase[i]);
+
+			try {
+				db.cancel(resToAdd[i]);
+			} catch (NonExistingReservationException dce) {
+				System.out.println("NonExistingReservation: " + dce.getMessage());
+				continue;
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+				continue;
+			}
+
+		}
+
+		teardown();
+	}
+
+	private static void testGetReservations() {
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println("________________________ TESTING getReservations() _______________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		Customer[] custToSearch = new DawsonCustomer[3];
+		String[] testCase = new String[3];
+
+		testCase[0] = new String("\nTest Case 1 Valid get Email Exist in the Database ");
+		custToSearch[0] = new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com");
+
+		testCase[1] = new String("\nTest Case 2 Invalid Email does not exist in the Database ");
+		custToSearch[1] = new DawsonCustomer("Dominica", "Esperente", "piquanteChiquita@hotmail.com");
+
+		for (int i = 0; i < custToSearch.length; i++) {
+			System.out.println(testCase[i]);
+
+			try {
+				db.getReservations(custToSearch[i]);
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+				continue;
+			}
+
+		}
+		teardown();
+	}
+
+	private static void testGetReservedRooms() {
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println("_______________________ TESTING getReservedRooms() ________________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		String[] testcase = new String[6];
+		Reservation[] resToSearch = new DawsonReservation[6];
+
+		testcase[0] = "\n Case 1: Valid Data";
+		resToSearch[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
+
+		testcase[1] = "\n Case 2: Invalid Data: Invalid dates in list";
+		resToSearch[1] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 1995, 10, 26, 1996, 12, 30);
+
+		for (int i = 0; i < resToSearch.length; i++) {
+			System.out.println(testcase[i]);
+
+			try {
+				db.getReservedRooms(resToSearch[i].getCheckInDate(), resToSearch[i].getCheckOutDate());
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+				continue;
+			}
+
+		}
+
+		teardown();
+	}
+
+	private static void testGetFreeRooms() {
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println("_______________________ TESTING getFreeRooms() ________________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		String[] testcase = new String[6];
+		Reservation[] resToSearch = new DawsonReservation[6];
+
+		testcase[0] = "\n Case 1: Valid Data";
+		resToSearch[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 1995, 10, 26, 1996, 12, 30);
+
+		testcase[1] = "\n Case 2: Invalid Data: Date used already";
+		resToSearch[1] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
+
+		for (int i = 0; i < resToSearch.length; i++) {
+			System.out.println(testcase[i]);
+
+			try {
+				db.getFreeRooms(resToSearch[i].getCheckInDate(), resToSearch[i].getCheckOutDate());
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+				continue;
+			}
+
+		}
+		teardown();
+	}
+
+	private static void testThreeParamsGetFreeRooms() {
+		setup();
+		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+		ReservationListDB db = new ReservationListDB(file);
+
+		System.out.println(
+				"_______________________ TESTING getFreeRooms() with three parameters ________________________\n");
+
+		System.out.println("\nLIST USED:");
+
+		System.out.println(db.toString());
+
+		String[] testcase = new String[6];
+		Reservation[] resToSearch = new DawsonReservation[6];
+
+		testcase[0] = "\n Case 1: Valid Data";
+		resToSearch[0] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 1995, 10, 26, 1996, 12, 30);
+
+		testcase[1] = "\n Case 2: Invalid Data: Date used already";
+		resToSearch[1] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
+
+		testcase[2] = "\n Case 3: Invalid Data: Wrong Room Type";
+		resToSearch[2] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
+				new DawsonRoom(105, RoomType.PENTHOUSE), 2008, 10, 5, 2010, 10, 5);
+
+		for (int i = 0; i < resToSearch.length; i++) {
+			System.out.println(testcase[i]);
+
+			try {
+				db.getFreeRooms(resToSearch[i].getCheckInDate(), resToSearch[i].getCheckOutDate(),
+						resToSearch[i].getRoom().getRoomType());
+			} catch (Exception e) {
+				System.out.println("<----HANDLE ME---> " + e.getMessage() + " <----HANDLE ME---> ");
+				continue;
+			}
+
+		}
+		teardown();
+	}
 
 }
