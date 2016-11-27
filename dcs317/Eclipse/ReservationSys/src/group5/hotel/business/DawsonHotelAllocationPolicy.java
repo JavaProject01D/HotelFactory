00bb30a50c96998c7c7ac774ec6535166d8dbe2c
Zahraa <Zahraa@25.110.238.213>
2016-11-27 15:32:16 -0500

    100% done and working.

--- dcs317/Eclipse/ReservationSys/src/group5/hotel/business/DawsonHotelAllocationPolicy.java
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/business/DawsonHotelAllocationPolicy.java
@@ -10,71 +10,86 @@ import dw317.hotel.business.RoomType;
 import dw317.hotel.business.interfaces.AllocationPolicy;
 import dw317.hotel.business.interfaces.Room;
 import dw317.hotel.data.interfaces.ReservationDAO;
-
+/**
+ * This class tries to spread out its guest evenly through the hotel according to a specific algorithm.
+ * @author Zahraa Horeibi
+ *
+ */
 public class DawsonHotelAllocationPolicy implements AllocationPolicy {
 
 	private static final long serialVersionUID = 42031768871L;
 	private ReservationDAO reservationDAO;
-	// NEW keyword?
 	
+	/** 
+	 * The DawsonHotelAllocationPolicy class gets a ReservationDAO object in 
+	 * its constructor and it keeps a reference to this object as an instance variable. 
+	 * @author Zahraa Horeibi
+	 * @param res
+	 */
 	public DawsonHotelAllocationPolicy( ReservationDAO res ) {
-		
 		this.reservationDAO = res;
 	}
-
-//heh
+	
+	/**
+	 * This method finds the floor with the most number of free rooms of the given room type during the desired period and 
+	 * randomly choose one of the free rooms on that floor. If there are no free rooms available with the search criteria, return an empty Optional
+	 * @author Zahraa Horeibi
+	 * @param checkin, checkout, roomType
+	 * @return Optional<Room>
+	 */
 	@Override
-	public Optional<Room> getAvailableRoom(LocalDate checkin, LocalDate checkout, RoomType roomType) {
-
-		
+	public Optional<Room> getAvailableRoom(LocalDate checkin, LocalDate checkout, RoomType roomType) {		
 		List<Room> freeRooms = this.reservationDAO.getFreeRooms(checkin, checkout, roomType);	
 		List<Room> freeRoomsOnFloor = new ArrayList<Room>();
-		int floorNum = searchFloor(freeRooms);
-		Random randomRoom = new Random();
 		
+		Room someFreeRoom = searchFloor(freeRooms);
+			
+			if (someFreeRoom == null)
+				return Optional.empty();
+			
+		int floorNum = someFreeRoom.getFloor();
+			// Assigning all the free rooms on the same floor to a list. 
 		for (int k = 0; k < freeRooms.size() ; k++ ) {
 			if (freeRooms.get(k).getFloor() == floorNum)
 				freeRoomsOnFloor.add(freeRooms.get(k));
 		}
-		
-		int rand = randomRoom.nextInt(((freeRoomsOnFloor.get(freeRoomsOnFloor.size() - 1).getRoomNumber() - freeRoomsOnFloor.get(0).getRoomNumber()) + 1) + freeRoomsOnFloor.get(0).getRoomNumber());
-
-		
-		
-		
-		
-		return Optional.ofNullable(freeRoomsOnFloor.get(0));
-	
+			Random rand = new Random();
+		 	int index = rand.nextInt(freeRoomsOnFloor.size());
+		 
+		 	//gets a random room using the random int value of index
+		 return Optional.of(freeRoomsOnFloor.get(index));
 	}
-
-	public static int searchFloor(List<Room> list) {
-
-
+/**
+ * This helper method returns the first room number of the floor which contains the most number of free rooms. 
+ * If multiple floors have the same positive number of available rooms, it choose the lowest floor.
+ * @author Zahraa Horeibi
+ * @param list
+ * @return Room
+ */
+	private static Room searchFloor(List<Room> list) {
 		int count = 0;
-		int tempCount;
-		Room room = list.get(0);
-		Room temp;
-
-		for (int i = 0; i < list.size() - 1; i++) {
-			tempCount = 0;
-			temp = list.get(i);
+		int tempCount = 0;
+		Room room = null;
+		Room temp = null;
+		try {
+		room = list.get(0);
+		//checks if the list is empty it throws an IndexOutOfBoundsException to inform the user
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("No rooms are available with given information.");
+		}
+			for (int i = 0; i < list.size() - 1; i++) {
+				tempCount = 0;
+				temp = list.get(i);
 
 			for (int j = 0; j < list.size(); j++) {
-
-				if ((list.get(i).getFloor()) == (list.get(j).getFloor())) {
-				tempCount++;
-	
-				}
-			} // end of j
-
+				if ((list.get(i).getFloor()) == (list.get(j).getFloor())) 
+					tempCount++;
+			} // end of j for loop
 			if (tempCount > count) {
-				room = temp;
 				count = tempCount;
-				
-			} // end of if
-		} // end of i 
-
-		return room.getFloor();
-
+				room = temp;		
+			} // end of if statement
+		} // end of i for loop 
+		return room;
 	}
 }
--- dcs317/Eclipse/ReservationSys/test/group5/hotel/business/DawsonHotelAllocationPolicyTest.java
+++ dcs317/Eclipse/ReservationSys/test/group5/hotel/business/DawsonHotelAllocationPolicyTest.java
@@ -19,103 +19,251 @@ public class DawsonHotelAllocationPolicyTest {
 		getAvailableRoomsTest();
 	}
 	
-	private static void setup() {
+		private static void setup() {
 
-		String[] rooms = new String[8];
-		rooms[0] = "206*normal";
-		rooms[1] = "401*normal";
-		rooms[2] = "406*normal";
-		rooms[3] = "503*normal";
-		rooms[4] = "601*normal";
-		rooms[5] = "704*suite";
-		rooms[6] = "801*penthouse";
-		rooms[7] = "101*normal";
-
-		String[] custs = new String[8];
-		custs[0] = "bob.b.y_lee@hotmail.ca*Bobby*Lee*mastercard*5458325441641567";
-		custs[1] = "madoriHu@host.com*Humico*Madori*mastercard*5233382411178726";
-		custs[2] = "Jean_Gero@gg.fr*Jean*Gerophar*visa*4929596474756407";
-		custs[3] = "MyNameIsSheila@localhost*Sheila*Kaif*AMEX*374425782767815";
-		custs[4] = "cuty.kathy.2008@yahoo.ca*Kathy*Perry**";
-		custs[5] = "robert.is-awesome@mail.me*Robert*Siri*Visa*4485011762777210";
-		custs[6] = "john.bussiness@gmail.ca*Johny-Laurence*Smith**";
-		custs[7] = "Umila-Gangee@local.ca*Umila*Gangee**";
-
-		String[] reservs = new String[8];
-		reservs[0] = "bob.b.y_lee@hotmail.ca*2016*8*30*2016*12*25*206";
-		reservs[1] = "madoriHu@host.com*2016*10*26*2016*12*30*401";
-		reservs[2] = "Jean_Gero@gg.fr*2016*11*28*2017*2*25*406";
-		reservs[3] = "MyNameIsSheila@localhost*2017*5*9*2017*6*1*503";
-		reservs[4] = "cuty.kathy.2008@yahoo.ca*2016*12*25*2017*1*1*601";
-		reservs[5] = "robert.is-awesome@mail.me*2017*1*1*2018*1*1*704";
-		reservs[6] = "john.bussiness@gmail.ca*2016*9*20*2016*9*26*801";
-		reservs[7] = "Umila-Gangee@local.ca*2017*5*5*2017*7*8*101";
-
-		File dir = new File("testfiles");
-		try {
-			if (!dir.exists()) {
-				dir.mkdirs();
-			}
-			ListUtilities.saveListToTextFile(rooms, "testfiles/testRooms.txt");
-			ListUtilities.saveListToTextFile(custs, "testfiles/testCustomers.txt");
-			ListUtilities.saveListToTextFile(reservs, "testfiles/testReservations.txt");
-		} catch (IOException io) {
-			System.out.println("Error creating file in setUp()");
-		}
-	}
-
-	private static void teardown() {
-		File theFile = new File("testfiles/testRooms.txt");
-		if (theFile.exists()) {
-			theFile.delete();
-		}
-		theFile = new File("testfiles/testCustomers.txt");
-		if (theFile.exists()) {
-			theFile.delete();
-		}
-		theFile = new File("testfiles/testReservations.txt");
-		if (theFile.exists()) {
-			theFile.delete();
-		}
-	}
+			String[] rooms = new String[10];
+			rooms[0] = "206*normal";
+			rooms[1] = "401*normal";
+			rooms[2] = "704*suite";
+			rooms[3] = "801*penthouse";
+			rooms[4] = "801*penthouse";
+			rooms[5] = "101*normal";
+			rooms[6] = "203*normal";
+			rooms[7] = "205*normal";
 	
+			String[] custs = new String[7];
+			custs[0] = "bbbb@aaaa*Bobby*Lee*mastercard*5458325441641567";
+			custs[1] = "bbbb@bbbb*Humico*Madori*mastercard*5233382411178726";
+			custs[2] = "iiiiii@iiii*Robert*Siri*Visa*4485011762777210";
+			custs[3] = "bbbb@jjjjj*Johny-Laurence*Smith**";
+			custs[4] = "yyyy@yyyy*Umila*Gangee**";
+			
+			String[] reservs = new String[7];
+			reservs[0] = "bbbb@aaaa*2016*8*30*2016*12*25*206";
+			reservs[1] = "bbbb@bbbb*2016*10*26*2016*12*30*401";
+			reservs[2] = "iiiiii@iiii*2017*1*1*2018*1*1*704";
+			reservs[3] = "bbbb@jjjjj*2016*9*20*2016*9*26*801";
+			reservs[4] = "yyyy@yyyy*2017*5*5*2017*7*8*801";
+			
+			File dir = new File("testfiles");
+			try {
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				ListUtilities.saveListToTextFile(rooms, "testfiles/testRooms.txt");
+				ListUtilities.saveListToTextFile(custs, "testfiles/testCustomers.txt");
+				ListUtilities.saveListToTextFile(reservs, "testfiles/testReservations.txt");
+			} catch (IOException io) {
+				System.out.println("Error creating file in setUp()");
+			}
+		}
+		private static void setupTwoRoomsSameFloor() {
+
+			String[] rooms = new String[15];
+			rooms[0] = "206*normal";
+			rooms[1] = "401*normal";
+			rooms[2] = "704*suite";
+			rooms[3] = "801*penthouse";
+			rooms[4] = "801*penthouse";
+			rooms[5] = "101*normal";
+			rooms[6] = "203*normal";
+			rooms[7] = "205*normal";
+			rooms[8] = "404*normal";
+			rooms[9] = "405*normal";
+
+			String[] custs = new String[7];
+			custs[0] = "bbbb@aaaa*Bobby*Lee*mastercard*5458325441641567";
+			custs[1] = "bbbb@bbbb*Humico*Madori*mastercard*5233382411178726";
+			custs[2] = "iiiiii@iiii*Robert*Siri*Visa*4485011762777210";
+			custs[3] = "bbbb@jjjjj*Johny-Laurence*Smith**";
+			custs[4] = "yyyy@yyyy*Umila*Gangee**";
+			
+			String[] reservs = new String[7];
+			reservs[0] = "bbbb@aaaa*2016*8*30*2016*12*25*206";
+			reservs[1] = "bbbb@bbbb*2016*10*26*2016*12*30*401";
+			reservs[2] = "iiiiii@iiii*2017*1*1*2018*1*1*704";
+			reservs[3] = "bbbb@jjjjj*2016*9*20*2016*9*26*801";
+			reservs[4] = "yyyy@yyyy*2017*5*5*2017*7*8*801";
+			
+			File dir = new File("testfiles");
+			try {
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				ListUtilities.saveListToTextFile(rooms, "testfiles/testRooms.txt");
+				ListUtilities.saveListToTextFile(custs, "testfiles/testCustomers.txt");
+				ListUtilities.saveListToTextFile(reservs, "testfiles/testReservations.txt");
+			} catch (IOException io) {
+				System.out.println("Error creating file in setUp()");
+			}
+		}
+		private static void setupBiggest() {
+
+			String[] rooms = new String[15];
+			rooms[0] = "206*normal";
+			rooms[1] = "401*normal";
+			rooms[2] = "704*suite";
+			rooms[3] = "801*penthouse";
+			rooms[4] = "801*penthouse";
+			rooms[5] = "101*normal";
+			rooms[6] = "203*normal";
+			rooms[7] = "205*normal";
+			rooms[8] = "404*normal";
+			rooms[9] = "405*normal";
+			rooms[10] = "502*normal";
+			rooms[11] = "503*normal";
+			rooms[12] = "505*normal";
+
+			String[] custs = new String[7];
+			custs[0] = "bbbb@aaaa*Bobby*Lee*mastercard*5458325441641567";
+			custs[1] = "bbbb@bbbb*Humico*Madori*mastercard*5233382411178726";
+			custs[2] = "iiiiii@iiii*Robert*Siri*Visa*4485011762777210";
+			custs[3] = "bbbb@jjjjj*Johny-Laurence*Smith**";
+			custs[4] = "yyyy@yyyy*Umila*Gangee**";
+			
+			String[] reservs = new String[7];
+			reservs[0] = "bbbb@aaaa*2016*8*30*2016*12*25*206";
+			reservs[1] = "bbbb@bbbb*2016*10*26*2016*12*30*401";
+			reservs[2] = "iiiiii@iiii*2017*1*1*2018*1*1*704";
+			reservs[3] = "bbbb@jjjjj*2016*9*20*2016*9*26*801";
+			reservs[4] = "yyyy@yyyy*2017*5*5*2017*7*8*801";
+			
+			File dir = new File("testfiles");
+			try {
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				ListUtilities.saveListToTextFile(rooms, "testfiles/testRooms.txt");
+				ListUtilities.saveListToTextFile(custs, "testfiles/testCustomers.txt");
+				ListUtilities.saveListToTextFile(reservs, "testfiles/testReservations.txt");
+			} catch (IOException io) {
+				System.out.println("Error creating file in setUp()");
+			}
+		}
+
+
+		private static void teardown() {
+			File theFile = new File("testfiles/testRooms.txt");
+			if (theFile.exists()) {
+				theFile.delete();
+			}
+			theFile = new File("testfiles/testCustomers.txt");
+			if (theFile.exists()) {
+				theFile.delete();
+			}
+			theFile = new File("testfiles/testReservations.txt");
+			if (theFile.exists()) {
+				theFile.delete();
+			}
+		}
 	private static void getAvailableRoomsTest() {
 		
-		setup();
+	/*	setup();
 		SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
 				"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
 
 		ReservationListDB db = new ReservationListDB(file);
 		DawsonHotelAllocationPolicy dhap = new DawsonHotelAllocationPolicy(db);
-		System.out.println(db.toString());
+		System.out.println(db.toString() + "\n");
+	*/	
+		String[] testcase = new String[8];
+		LocalDate[] checkin = new LocalDate[8];
+		LocalDate[] checkout = new LocalDate[8];
+		RoomType[] roomType = new RoomType[8];
 		
-		String[] testcase = new String[6];
-		testcase[0] = "Case 1: Valid data";
+		testcase[0] = "Case 1: Valid data -- No rooms available with given information";
+		checkin[0] = LocalDate.of(2016, 9, 20);
+		checkout[0] = LocalDate.of(2016, 9, 26);
+		roomType[0] = RoomType.PENTHOUSE;
+		
+		testcase[1] = "Case 2: Valid data -- One room available on one floor";
+		checkin[1] = LocalDate.of(2016, 7, 20);
+		checkout[1] = LocalDate.of(2016, 11, 26);
+		roomType[1] = RoomType.NORMAL;
+		
+		testcase[2] = "Case 3: Valid data -- Same amount of rooms available on two different floors";
+		checkin[2] = LocalDate.of(2016, 10, 26);
+		checkout[2] = LocalDate.of(2016, 12, 30);
+		roomType[2] = RoomType.NORMAL;
 
+		testcase[3] = "Case 4: Valid data -- More than one room available on one floor";
+		checkin[3] = LocalDate.of(2016, 10, 20);
+		checkout[3] = LocalDate.of(2016, 11, 26);
+		roomType[3] = RoomType.NORMAL;
 		
-	//	resToSearch[1] = new DawsonReservation(new DawsonCustomer("Habiba", "Awada", "habiba_awad@hotmail.com"),
-	//			new DawsonRoom(105, RoomType.NORMAL), 2008, 10, 5, 2010, 10, 5);
+	//	Optional<Room> r = dhap.getAvailableRoom(checkin[3], checkout[3], roomType[3]);
+	//	System.out.println("List of Free Rooms: " + db.getFreeRooms(checkin[3], checkout[3], roomType[3]));
+	//	System.out.println("Chosen Free Room: " + r.get() + "\n");
 		
-		LocalDate checkin = LocalDate.of(2016, 12, 25);
-		LocalDate checkout = LocalDate.of(2017, 1, 1);
-		//heh
-	
-		Optional<Room> r = dhap.getAvailableRoom(checkin, checkout, RoomType.PENTHOUSE);
-		System.out.println(r);
-		
-	/*	for (int i = 0; i < resToSearch.length; i++) {
+		for (int i = 0; i < 4; i++) {
+			if (i == 0 || i == 1) {
+			setup();
+			SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+					"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+
+			ReservationListDB db = new ReservationListDB(file);
+			DawsonHotelAllocationPolicy dhap = new DawsonHotelAllocationPolicy(db);
+			
 			System.out.println(testcase[i]);
 			try {
-			 r =  dhap.getAvailableRoom(resToSearch[i].getCheckInDate(), resToSearch[i].getCheckOutDate(), resToSearch[i].getRoom().getRoomType());
-				System.out.println(r);
+	
+				Optional<Room>r = dhap.getAvailableRoom(checkin[i], checkout[i], roomType[i]);
+				System.out.println("\t List of Free Rooms: " + db.getFreeRooms(checkin[i], checkout[i], roomType[i]));
+				System.out.println("\t Chosen Free Room: " + r.get() + "\n");
+					
 			} catch (Exception e) {
-				System.out.println("Fix me: " + e.getMessage());
+				System.out.println("\t\t Error: " + e.getMessage() + "\n");
 				continue;
 			}
-			System.out.println(r.toString());
-		} */
-		teardown();
+			teardown();
+		   }
+			
 		
-		
+			if ( i == 2) {
+			
+					setupTwoRoomsSameFloor();
+					SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+							"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+
+					ReservationListDB db = new ReservationListDB(file);
+					DawsonHotelAllocationPolicy dhap = new DawsonHotelAllocationPolicy(db);
+					
+					System.out.println(testcase[i]);
+					try {
+			
+						Optional<Room>r = dhap.getAvailableRoom(checkin[i], checkout[i], roomType[i]);
+						System.out.println("\t List of Free Rooms: " + db.getFreeRooms(checkin[i], checkout[i], roomType[i]));
+						System.out.println("\t Chosen Free Room: " + r.get() + "\n");
+							
+					} catch (Exception e) {
+						System.out.println("\t\t Error: " + e.getMessage() + "\n");
+						continue;
+					}
+					teardown();
+				}
+			
+			if (i == 3) {
+					setupBiggest();
+					SequentialTextFileList file = new SequentialTextFileList("testfiles/testRooms.txt",
+							"testfiles/testCustomers.txt", "testfiles/testReservations.txt");
+
+					ReservationListDB db = new ReservationListDB(file);
+					DawsonHotelAllocationPolicy dhap = new DawsonHotelAllocationPolicy(db);
+					
+					System.out.println(testcase[i]);
+					try {
+			
+						Optional<Room>r = dhap.getAvailableRoom(checkin[i], checkout[i], roomType[i]);
+						System.out.println("\t List of Free Rooms: " + db.getFreeRooms(checkin[i], checkout[i], roomType[i]));
+						System.out.println("\t Chosen Free Room: " + r.get() + "\n");
+							
+					} catch (Exception e) {
+						System.out.println("\t\t Error: " + e.getMessage() + "\n");
+						continue;
+					}
+					teardown();
+				 
+			}
+		} 
 	}
 }
