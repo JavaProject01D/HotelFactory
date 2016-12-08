6251033554791089ce3efe2d293331b8cf08bc83
1537595 <1537595@2F14-30.ad.dawsoncollege.qc.ca>
2016-12-08 11:02:32 -0500

    working on part 2

---
+++ dcs317/Eclipse/ReservationSys/src/group5/hotel/ui/TextView.java
@@ -0,0 +1,14 @@
+package group5.hotel.ui;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class TextView implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
